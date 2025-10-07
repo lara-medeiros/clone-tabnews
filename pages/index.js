c564d56ebb4664afc25722c51b312979166ddbd2
Lara Medeiros <laramedeiros695@gmail.com>
2025-10-07 15:11:25 +0000

    refactor:modificar a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Obrigada por sempre me impulsionar, me ensinar e me ajudar a me tornar minha melhor versão!</h1>
+    return <h1>Obrigada por sempre me impulsionar, me ensinar e me ajudar a me tornar minha melhor versão!❤️</h1>
 }
 
 export default Home;
