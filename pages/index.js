e1bd676953e28532f0e9805f44832c0100c8e3e9
Lara Medeiros <laramedeiros695@gmail.com>
2025-10-29 13:18:32 +0000

    feat: adicionar arquivos `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Obrigada por sempre me impulsionar, me ensinar e me ajudar a me tornar minha melhor versão!❤️</h1>
+  return (
+    <h1>
+      Obrigada por sempre me impulsionar, me ensinar e me ajudar a me tornar
+      minha melhor versão!❤️
+    </h1>
+  );
 }
 
 export default Home;
